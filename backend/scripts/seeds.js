63eff39a3fc1de4ac24b52364668d948c05332a4
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-02-15 07:52:54 +0000

    fix: correctly search for entity

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -15,13 +15,13 @@ const Comment = mongoose.model("Comment");
 const NUM_USER = 100
 const NUM_ITEM = 100
 const NUM_COMMENT = 100
-const FIRST_USER_ID = 'firstuser'
-const FIRST_ITEM = 'firstitem'
+const FIRST_USERNAME = 'firstuser1'
+const FIRST_ITEM_TITLE = 'firstitem1'
 
-function seedUsers() {
+async function seedUsers() {
   for (let i = 0; i < NUM_USER; i++) {
     const user = new User();
-    const username = i === 0 ? FIRST_USER_ID : crypto.randomUUID().replaceAll('-', '').substring(0, 20)
+    const username = i === 0 ? FIRST_USERNAME : crypto.randomUUID().replaceAll('-', '').substring(0, 20)
     const email = `${username.substring(0, 10)}@example.com`
     const password = '123456'
 
@@ -29,61 +29,57 @@ function seedUsers() {
     user.email = email;
     user.setPassword(password);
 
-    user.save()
+    await user.save()
   }
 }
 
-function seedItems() {
-  User.findById(FIRST_USER_ID)
-    .then(function (user) {
-      if (!user) {
-        console.error("User cannot be found")
-        return
-      }
+async function seedItems() {
+  const user = await User.findOne({ username: FIRST_USERNAME })
 
-      for (let i = 0; i < NUM_ITEM; i++) {
-        const text = crypto.randomUUID().replaceAll('-', '').substring(0, 20)
-        const item = new Item({
-          id: i === 0 ? FIRST_ITEM : undefined,
-          title: text,
-          description: text,
-        });
+  if (!user) {
+    console.error("User cannot be found")
+    return
+  }
 
-        item.seller = user;
+  for (let i = 0; i < NUM_ITEM; i++) {
+    const text = crypto.randomUUID().replaceAll('-', '').substring(0, 20)
+    const item = new Item({
+      title: i === 0 ? FIRST_ITEM_TITLE : text,
+      description: text,
+    });
 
-        item.save()
-      }
-    })
-    .catch(err => console.error(err));
+    item.seller = user;
+
+    await item.save()
+  }
 }
 
-function seedComments() {
-  Item.findById(FIRST_ITEM)
-    .then(function (item) {
-      if (!item) {
-        console.error("Item cannot be found")
-        return
-      }
+async function seedComments() {
+  const item = await Item.findOne({ title: FIRST_ITEM_TITLE })
 
-      for (let i = 0; i < NUM_COMMENT; i++) {
-        const text = crypto.randomUUID().replaceAll('-', '').substring(0, 20)
-        const comment = new Comment({
-          body: text,
-          seller: item.seller,
-          item: item
-        })
+  if (!item) {
+    console.error("Item cannot be found")
+    return
+  }
 
-        comment.save()
-      }
+  for (let i = 0; i < NUM_COMMENT; i++) {
+    const text = crypto.randomUUID().replaceAll('-', '').substring(0, 20)
+    const comment = new Comment({
+      body: text,
+      seller: item.seller,
+      item: item
     })
-    .catch(err => console.error(err));
+
+    await comment.save()
+  }
+
 }
 
-function seedDB() {
-  seedUsers()
-  seedItems()
-  seedComments()
-  
+async function seedDB() {
+  await seedUsers()
+  await seedItems()
+  await seedComments()
+
   mongoose.connection.close()
 }
 
