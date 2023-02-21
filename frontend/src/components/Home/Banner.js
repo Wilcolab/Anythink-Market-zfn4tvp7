2e84ef6545663f4fb9fe050229db1161a02435d6
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-02-21 22:40:09 +0900

    Feature/search (#5)
    * feat: search items by title
    
    * fix: contains instead of exact match
    
    * feat: search on home

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
+import { connect } from "react-redux";
+import { APPLY_TAG_FILTER } from "../../constants/actionTypes";
 
-const Banner = () => {
+const MINIMUM_CHARACTERS = 3
+
+const mapDispatchToProps = (dispatch) => ({
+  onSearchChange: (payload) =>
+    dispatch({ type: APPLY_TAG_FILTER, payload }),
+});
+
+const Banner = (props) => {
+  const [searchText, setSearchText] = useState('');
+
+  function searchTextChangeHandler(ev) {
+    const newSearchText = ev.target.value
+    setSearchText(newSearchText)
+
+    if (newSearchText.length >= MINIMUM_CHARACTERS) {
+      agent.Items.byTitle(newSearchText).then(items => {
+        props.onSearchChange(items)
+      }).catch(err => console.error(err))
+    }
+  }
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -9,6 +32,7 @@ const Banner = () => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
+          <input id="search-box" value={searchText} onChange={searchTextChangeHandler} placeholder="What is it that you truly desire?" type="text" />
           <span> the cool stuff.</span>
         </div>
       </div>
@@ -16,4 +40,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export default connect(() => ({}), mapDispatchToProps)(Banner);
